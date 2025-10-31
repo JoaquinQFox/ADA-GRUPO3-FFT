28147d1e77398f9012f7619f21dd04abad0c6761
Fernando Gerson Luque Guevara <fluqueg@unsa.edu.pe>
2025-10-31 16:07:31 -0500

    Agregando implementacion de la fft en fft.py
    Se implenmenta fft utilizando el algoritmo de Cooley-Tukey para señales

--- src/fft.py
+++ src/fft.py
@@ -1,1 +1,28 @@
-# Implementación de la transformada rápidad de fourier (FFT)
+import math
+import cmath
+
+# fft de coleyu tukey
+def fft(signals: list[complex]):
+    N = len(signals)
+    if N <= 1:
+        return signals
+    
+    # fft de las muestras pares e impares
+    par = fft(signals[0::2])
+    impar = fft(signals[1::2])
+    
+    # calcular los factores twiddle
+    T = [cmath.exp(-2j * math.pi * k / N) * impar[k] for k in range(N // 2)]
+    
+    #combinar los resultados
+    return [par[k] + T[k] for k in range(N // 2)] + \
+           [par[k] - T[k] for k in range(N // 2)]
+
+if __name__ == "__main__":
+    # Señal de prueba: suma de dos curvas  senoidales
+    signal = [math.sin(2*math.pi*1*t/32)+0.5*math.sin(2*math.pi*3*t/32) for t in range(32)]
+    
+    result = fft(signal)
+    
+    for i, val in enumerate(result):
+        print(f"X[{i}] = {val.real:.4f} + {val.imag:.4f}j")
