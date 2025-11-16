c4f9930e746c7546785209805d512f86ca59b367
leonhatches <jhatches@unsa.edu.pe>
2025-11-15 20:04:06 -0500

    Se crea la estructura básica para verificar los tiempos de ejecución

--- src/grafico.py
+++ src/grafico.py
@@ -1,1 +1,29 @@
 # Graficos comparativos de tiempos de ejecución de DFT y FFT
+
+# ----------------------------------------------------------------
+
+import time
+import math
+from dft import dft
+from fft import fft
+# from generar_signal import times
+
+# --------------------- Tiempos de Ejecución ---------------------
+
+# función de prueba
+def generar_signal (N):
+    return []
+
+# Medición de tiempos
+def medir_tiempos():
+    return 0, 0, 0 # size, tiempos_dft, tiempos_fft
+
+# Main
+if __name__ == "__main__":
+    medir_tiempos()
+
+# ----------------------------------------------------------------
+
+
+
+
